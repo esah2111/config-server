8a547ec58df721fb75a839560154604eb5c2e788
Mohammad Esah <esah2111@gmail.com>
2020-04-14 12:14:37 +0530

    Initial Commit

---
+++ src/test/java/com/esah2111/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esah2111.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
